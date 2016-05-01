370120d277102fd6a9fdace7554c8edc9bf34ebb
Todd <toddthetoad95@gmail.com>
2016-05-01 16:30:56 -0500

    preprocessor fixes

--- app/src/main/java/com/example/status/ece420finalprojecttake2/Preprocessor.java
+++ app/src/main/java/com/example/status/ece420finalprojecttake2/Preprocessor.java
@@ -25,7 +25,6 @@ public class Preprocessor {
         /********** Reading Input image **********/
 
         // Load an image from file - change this based on your image name
-//        IplImage pInpImg = cvLoadImage(filename);
         Mat pInpImg = Highgui.imread(filename);
         if (pInpImg == null) {
             Log.e("Image Error", "failed to load input image\n");
@@ -64,10 +63,6 @@ public class Preprocessor {
         Log.d("Image Conversion", "Thresholded image saved");
 
         /**********  Finding contours  **********/
-//        vector<vector<Point> > contours;
-//        vector<Vec4i> hierarchy;
-//        findContours(thresh, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE);
-
         List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
         Mat hierarchy = new Mat();
 
@@ -75,25 +70,6 @@ public class Preprocessor {
         Log.d("Contours", "Contours Found: "+contours.size());
 
         /**********  Finding largest countour **********/
-        //cout << "# of contours dectected: " << contours.size() << endl;
-
-//        double largest_area=100;
-//        int largest_contour_index=0;
-//        double peri = 0;
-//        vector<Point> approxCurve;
-//
-//        Rect bounding_rect;
-//        for( int i = 0; i< contours.size(); i++ ){
-//            //Find the area of contour
-//            double a=contourArea(contours[i],false);
-//            if(a>largest_area){
-//                largest_area=a;
-//                //Store the index of largest contour
-//                largest_contour_index=i;
-//                approxPolyDP(contours[i], approxCurve, 0.05*arcLength(contours[i], true), true);
-//                bounding_rect=boundingRect(contours[i]); // Find the bounding rectangle for biggest contour
-//            }
-//        }
 
         double largest_area=100;
         int largest_contour_index=0;
@@ -123,7 +99,6 @@ public class Preprocessor {
         Scalar white = new Scalar(255,255,255);
         Scalar red = new Scalar(0,0,255);
         Scalar blue = new Scalar(255,0,0);
-//        //cout << "Curve size: " << approxCurve.size() << endl;
 
         Log.d("Contours", "Curve size" + approxCurve.size());
 
@@ -134,30 +109,6 @@ public class Preprocessor {
 
 
         /**********  Draw around largest rectangle **********/
-//        Mat dst = thresh;
-//
-//        // Draw the largest contour using previously stored index.
-//        drawContours(dst, contours,largest_contour_index, color, CV_FILLED, 8, hierarchy);
-//        vector<Point> largestrect(4);
-//        if (approxCurve.size() >= 4 && (approxCurve.size() <= 10)){
-//            largestrect = findLargestQuad(approxCurve);
-//        }
-//
-//        if(!largestrect.empty()){
-//            //cout << "Largest Rect Size: " << (double)contourArea(largestrect,false) << endl;
-//            for(int i = 0; i < largestrect.size(); i ++){
-//                //line(orig, approxCurve.at(i%approxCurve.size()), approxCurve.at((i+1)%approxCurve.size()), red, thickness);
-//                line(orig, largestrect.at(i%largestrect.size()), largestrect.at((i+1)%largestrect.size()), red, thickness);
-//            }
-//        }
-//        else{
-//            cout << "No suitable rectangle detected" << endl;
-//        }
-
-        //Bounding rectangle (For testing)
-        //rectangle(orig, bounding_rect,  white, thickness, 8,0);
-
-//        output = orig;
 
         Mat dst = thresh;
 
@@ -193,9 +144,6 @@ public class Preprocessor {
 
         /**********  Writing output image **********/
         // using a different image format -- .png
-//        if(!imwrite(argv[2], output)){
-//            fprintf(stderr, "failed to write image file\n");
-//        }
         Highgui.imwrite(filename + "processed.jpg", dst);
         Highgui.imwrite(filename + "processed2.jpg", orig);
         Log.d("Preprocessor", "Image: " + orig);
@@ -249,255 +197,13 @@ public class Preprocessor {
         Highgui.imwrite(filename + "processed3_5.jpg", greyCroppedImage);
 
         //Use adaptive threshhold to binarize image
-        Imgproc.adaptiveThreshold(greyCroppedImage, cropped_binary, 255, Imgproc.ADAPTIVE_THRESH_GAUSSIAN_C, Imgproc.THRESH_BINARY, 601, 80);
+        int blocksize = (greyCroppedImage.width()/12)*2 + 1;
+        Imgproc.adaptiveThreshold(greyCroppedImage, cropped_binary, 255, Imgproc.ADAPTIVE_THRESH_GAUSSIAN_C, Imgproc.THRESH_BINARY, blocksize, 80);
         // resize(cropped_binary, cropped_binary, SIZE??, 0, 0, interpolation);
         output = cropped_binary;
 
         Highgui.imwrite(filename + "processed4.jpg", cropped_binary);
 
-        /**********  Freeing Memory **********/
-//        Core.ReleaseImage(pInpImg);
-        return;
-
-    }
-
-    public static void preprocess_test(String filename, Mat pInpImg) {
-        /********** Reading Input image **********/
-
-        // Load an image from file - change this based on your image name
-//        IplImage pInpImg = cvLoadImage(filename);
-//        Mat pInpImg = Imgcodecs.imread(filename);
-//        if (pInpImg == null) {
-//            Log.e("Image Error", "failed to load input image\n");
-//            return;
-//        }
-//        else {
-//            Log.d("Image Loading", "Image Loaded Successfully");
-//        }
-
-        //Convert to mat (default is mat)
-        Mat orig = pInpImg;  // default additional arguments: don't copy data.
-        Mat output = orig;
-
-        Log.d("Image Conversion", "Image Converted to Mat");
-
-        /**********  Code for contour detection **********/
-        //Convert to grayscale
-        Mat gray = new Mat();
-        Imgproc.cvtColor(orig, gray, Imgproc.COLOR_BGR2GRAY);
-
-        Log.d("Image Conversion", "Image Converted to Gray");
-
-        //Gaussian blur to remove noise
-        Imgproc.GaussianBlur(gray, gray, new Size(5, 5), 0, 0, Imgproc.BORDER_DEFAULT);
-
-        Log.d("Image Conversion", "Image Blurred");
-
-        //Create threshold image
-        Mat thresh = new Mat();
-        Imgproc.adaptiveThreshold(gray, thresh, 255, 1, 1, 11, 2);
-
-        Log.d("Image Conversion", "Image Thresholded");
-
-        Highgui.imwrite(filename + "greyandthresh.jpg", thresh);
-
-        Log.d("Image Conversion", "Thresholded image saved");
-
-        /**********  Finding contours  **********/
-//        vector<vector<Point> > contours;
-//        vector<Vec4i> hierarchy;
-//        findContours(thresh, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE);
-
-        List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
-        Mat hierarchy = new Mat();
-
-        Imgproc.findContours(thresh,contours,hierarchy,Imgproc.RETR_TREE,Imgproc.CHAIN_APPROX_SIMPLE);
-        Log.d("Contours", "Contours Found: "+contours.size());
-
-        /**********  Finding largest countour **********/
-        //cout << "# of contours dectected: " << contours.size() << endl;
-
-//        double largest_area=100;
-//        int largest_contour_index=0;
-//        double peri = 0;
-//        vector<Point> approxCurve;
-//
-//        Rect bounding_rect;
-//        for( int i = 0; i< contours.size(); i++ ){
-//            //Find the area of contour
-//            double a=contourArea(contours[i],false);
-//            if(a>largest_area){
-//                largest_area=a;
-//                //Store the index of largest contour
-//                largest_contour_index=i;
-//                approxPolyDP(contours[i], approxCurve, 0.05*arcLength(contours[i], true), true);
-//                bounding_rect=boundingRect(contours[i]); // Find the bounding rectangle for biggest contour
-//            }
-//        }
-
-        double largest_area=100;
-        int largest_contour_index=0;
-        double peri = 0;
-        MatOfPoint2f approxCurve = new MatOfPoint2f();
-
-        Rect bounding_rect = new Rect();
-        for( int i = 0; i< contours.size(); i++ ){
-            //Find the area of contour
-            double a=Imgproc.contourArea(contours.get(i),false);
-            if(a>largest_area){
-                largest_area=a;
-                //Store the index of largest contour
-                largest_contour_index=i;
-                /// New variable
-                MatOfPoint2f contours2f = new MatOfPoint2f( contours.get(i).toArray() );
-                Imgproc.approxPolyDP(contours2f, approxCurve, 0.05*Imgproc.arcLength(contours2f, true), true);
-                bounding_rect=Imgproc.boundingRect(contours.get(i)); // Find the bounding rectangle for biggest contour
-            }
-        }
-        //cout << "Size of largest area: " << (double)largest_area << endl;
-        Log.d("Contours", "Largest Area: "+largest_area);
-
-        /**********  Constants for drawing  **********/
-        int thickness = 20;
-        Scalar color = new Scalar(128,128,255);
-        Scalar white = new Scalar(255,255,255);
-        Scalar red = new Scalar(0,0,255);
-        Scalar blue = new Scalar(255,0,0);
-//        //cout << "Curve size: " << approxCurve.size() << endl;
-
-        Log.d("Contours", "Curve size" + approxCurve.size());
-
-        /**********  Draw around contour (Testing only)  **********/
-        for(int i = 0; i < approxCurve.toList().size(); i++){
-            Core.line(orig, approxCurve.toList().get(i%approxCurve.toList().size()), approxCurve.toList().get((i+1)%approxCurve.toList().size()), blue, thickness);
-        }
-
-
-        /**********  Draw around largest rectangle **********/
-//        Mat dst = thresh;
-//
-//        // Draw the largest contour using previously stored index.
-//        drawContours(dst, contours,largest_contour_index, color, CV_FILLED, 8, hierarchy);
-//        vector<Point> largestrect(4);
-//        if (approxCurve.size() >= 4 && (approxCurve.size() <= 10)){
-//            largestrect = findLargestQuad(approxCurve);
-//        }
-//
-//        if(!largestrect.empty()){
-//            //cout << "Largest Rect Size: " << (double)contourArea(largestrect,false) << endl;
-//            for(int i = 0; i < largestrect.size(); i ++){
-//                //line(orig, approxCurve.at(i%approxCurve.size()), approxCurve.at((i+1)%approxCurve.size()), red, thickness);
-//                line(orig, largestrect.at(i%largestrect.size()), largestrect.at((i+1)%largestrect.size()), red, thickness);
-//            }
-//        }
-//        else{
-//            cout << "No suitable rectangle detected" << endl;
-//        }
-
-        //Bounding rectangle (For testing)
-        //rectangle(orig, bounding_rect,  white, thickness, 8,0);
-
-//        output = orig;
-
-        Mat dst = thresh;
-
-        // Draw the largest contour using previously stored index.
-        Imgproc.drawContours(dst, contours,largest_contour_index, color, Core.FILLED);
-        List<Point> largestrect = new ArrayList<>(4);
-        if (approxCurve.toList().size() >= 4 && (approxCurve.toList().size() <= 10)){
-            largestrect = findLargestQuad(approxCurve.toList());
-        }
-
-        Log.d("Contours","largest rect data size: "+ largestrect.size() + "largest rect:" + largestrect);
-
-        if(largestrect.size() != 0){
-            //cout << "Largest Rect Size: " << (double)contourArea(largestrect,false) << endl;
-            MatOfPoint largestrect_mat = new MatOfPoint();
-            largestrect_mat.fromList(largestrect);
-            Log.d("Countours", "Largest Rect Size" + Imgproc.contourArea(largestrect_mat,false));
-            for(int i = 0; i < largestrect.size(); i ++){
-                //line(orig, approxCurve.at(i%approxCurve.size()), approxCurve.at((i+1)%approxCurve.size()), red, thickness);
-                Core.line(orig, largestrect.get(i%largestrect.size()), largestrect.get((i+1)%largestrect.size()), red, thickness);
-            }
-        }
-        else{
-            Log.e("Contours","NO SUITABLE RECTANGLE DETECTED");
-            return;
-        }
-
-        //Bounding rectangle (For testing) (not easy to translate)
-//        Imgproc.rectangle(orig, bounding_rect,  white, thickness, 8,0);
-
-//        output = orig;
-
-
-        /**********  Writing output image **********/
-        // using a different image format -- .png
-//        if(!imwrite(argv[2], output)){
-//            fprintf(stderr, "failed to write image file\n");
-//        }
-        Highgui.imwrite(filename + "processed.jpg", dst);
-        Highgui.imwrite(filename + "processed2.jpg", orig);
-        Log.d("Preprocessor", "Image: " + orig);
-
-        /**********  Map contour rectangle to bounding rectangle **********/
-        List<Point> bounding_rectangle_points = new ArrayList<>();
-        bounding_rectangle_points.add(new Point(bounding_rect.x, bounding_rect.y+bounding_rect.height));
-        bounding_rectangle_points.add(new Point(bounding_rect.x+bounding_rect.width, bounding_rect.y+bounding_rect.height));
-        bounding_rectangle_points.add(new Point(bounding_rect.x+bounding_rect.width, bounding_rect.y));
-        bounding_rectangle_points.add(new Point(bounding_rect.x, bounding_rect.y));
-
-//        vector<Point2f> rectangle_float = rectangletoPoint2f(largestrect);
-
-        largestrect = rectangletoPoint2f(largestrect);
-
-//        Mat transmtx = new Mat(4,1,Imgproc.CV_32FC2);
-        Mat transmtx = new Mat(4,1,CvType.CV_32FC2);
-        MatOfPoint largestrect_mat = new MatOfPoint();
-        largestrect_mat.fromList(largestrect);
-        largestrect_mat.convertTo(largestrect_mat,CvType.CV_32FC2);
-        MatOfPoint bounding_rectangle_points_mat = new MatOfPoint();
-        bounding_rectangle_points_mat.fromList(bounding_rectangle_points);
-        bounding_rectangle_points_mat.convertTo(bounding_rectangle_points_mat,CvType.CV_32FC2);
-        Log.d("Warping","Largest Rect: " + largestrect + "Bounding Rect: " + bounding_rectangle_points);
-        transmtx = Imgproc.getPerspectiveTransform(largestrect_mat/*rectangle_float*/,bounding_rectangle_points_mat);
-        Mat transformed = new Mat();
-        Imgproc.warpPerspective(orig, transformed, transmtx, orig.size());
-
-
-        /**********  Crop Image **********/
-        bounding_rect.x += 0.035*bounding_rect.width;
-        bounding_rect.y += 0.035*bounding_rect.height;
-        bounding_rect.width -= 0.07*bounding_rect.width;
-        bounding_rect.height -= 0.07*bounding_rect.height;
-        Mat croppedImage = transformed.submat(bounding_rect);
-
-        output = croppedImage;
-
-        Highgui.imwrite(filename + "processed3.jpg", croppedImage);
-
-        /**********  Binarize Image **********/
-        //Convert to black and white
-        Mat greyCroppedImage = new Mat();
-        Imgproc.cvtColor(croppedImage, greyCroppedImage, Imgproc.COLOR_BGR2GRAY);
-        Mat cropped_binary = new Mat();
-        greyCroppedImage.convertTo(greyCroppedImage, CvType.CV_8UC1);
-
-        //Median Blur to remove speckles and smooth
-        Imgproc.medianBlur(greyCroppedImage, greyCroppedImage, 13);
-
-        int blocksize = (greyCroppedImage.width()/6)*2 + 1;
-
-        //Use adaptive threshhold to binarize image
-        Imgproc.adaptiveThreshold(greyCroppedImage, cropped_binary, 255, Imgproc.ADAPTIVE_THRESH_GAUSSIAN_C, Imgproc.THRESH_BINARY, blocksize, 10);
-
-        // resize(cropped_binary, cropped_binary, SIZE??, 0, 0, interpolation);
-        output = cropped_binary;
-
-        Highgui.imwrite(filename + "processed4.jpg", cropped_binary);
-
-        /**********  Freeing Memory **********/
-//        Core.ReleaseImage(pInpImg);
         return;
 
     }
@@ -648,6 +354,8 @@ public class Preprocessor {
         Log.d("BINARY", "Image: " + testImage);
 
 //        preprocess(fileUri.toString());
-        preprocess_test(mediaFile.getAbsolutePath(), testImage);
+
+        //I DELTED THIS - TODD
+        //preprocess_test(mediaFile.getAbsolutePath(), testImage);
     }
 }
